bc90c0cfcdbd4d98ca4969a824cc4b2fa89fe208
Leon Yip <lyip1992@gmail.com>
2015-04-24 19:05:48 -0700

    Finished stringifyJSON.js

--- src/stringifyJSON.js
+++ src/stringifyJSON.js
@@ -4,5 +4,33 @@
 // but you don't so you're going to write it from scratch:
 
 var stringifyJSON = function(obj) {
-  // your code goes here
+	
+	var container, i, j, key, value;
+
+	switch (typeof obj) {
+
+		case 'string':
+			return '"' + obj + '"';
+
+		case 'number':
+		case 'boolean':
+			return String(obj);
+
+		case 'object':
+			if (!obj) return 'null';
+
+			container = [];
+			if (Object.prototype.toString.apply(obj) === '[object Array]') {
+				for (i = 0; i < obj.length; i++)
+					container[i] = stringifyJSON(obj[i]);
+				return '[' + container.join(',') + ']';
+			} else {
+				for (var j in obj) {
+					key = stringifyJSON(j);
+					value = stringifyJSON(obj[j]);
+					if (value) container.push(key + ':' + value);
+				}
+				return '{' + container.join(',') + '}';
+			}
+	}
 };
